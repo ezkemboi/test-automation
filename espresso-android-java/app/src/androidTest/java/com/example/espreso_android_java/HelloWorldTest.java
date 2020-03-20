1bf629bcd91795402b519edc399eb1d0452a3830
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-20 22:58:13 +0300

    update espresso tests

--- espresso-android-java/app/src/androidTest/java/com/example/espreso_android_java/HelloWorldTest.java
+++ espresso-android-java/app/src/androidTest/java/com/example/espreso_android_java/HelloWorldTest.java
@@ -2,9 +2,16 @@ package com.example.espreso_android_java;
 
 import android.content.Context;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,7 +23,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class HelloWorldTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello World")).check(matches(isDisplayed()));
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
