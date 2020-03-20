0ffa31d7026a7dd267d4326e2cfbecdaa82ea253
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-20 11:15:42 +0300

    add espresson sample cheetsheet code

---
+++ cheetsheet/espresso/flow.java
@@ -0,0 +1,14 @@
+/**
+This is how espresso workflow do 
+-> match -> act -> assert
+*/
+
+@Test 
+public void espressoFlow() {
+  // Need to perform an action
+  onview(withId(R.id.button))
+    .perform(click())
+  // check an action performed if it get the results required
+  onview(withId(R.id.results_textview))
+    .check(matches(withText("The button was clicked")))
+}
---
+++ cheetsheet/espresso/greetingsample.java
@@ -0,0 +1,12 @@
+@Test 
+public void greetingSample() {
+  // fill input field
+  onView(withId(R.id.name_field))
+    .perform(typeText("Kemboi"))
+  // click submit button
+  onView(withId(R.id.greet_button))
+    .perform(click())
+  // check output
+  onView(withText("Hello Kemboi!"))
+    .check(matches(isDisplayed()))
+}
