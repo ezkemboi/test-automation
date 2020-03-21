b004dda17d3f794a3fedaf8aea1ddbb97092a17d
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-21 13:58:31 +0300

    clean up angular app code

--- jasmine-protractor-angular/src/app/app-routing.module.ts
+++ jasmine-protractor-angular/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+// not found page should be added for redirect
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
   }
 ];
 
