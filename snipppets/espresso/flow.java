64ff1e21710bbf6fa44247f98b15f30bdf57bb31
me-x-mi <ezrqnkemboi@gmail.com>
2020-03-22 11:48:32 +0300

    add more notes on espresso usage

--- snipppets/espresso/flow.java
+++ snipppets/espresso/flow.java
@@ -5,9 +5,28 @@
 public class ExpressoFlow {
   @Test
   public void espressoFlow() {
-    // Need to perform an action
-    onview(withId(R.id.button)).perform(click());
-    // check an action performed if it get the results required
-    onview(withId(R.id.results_textview)).check(matches(withText("The button was clicked")));
+    /**
+     * onView() is a ViewMatcher. Then, click() is a ViewAction and
+     * matches(isDisplayed()) is a ViewAssertion
+     */
+    onview(withId(R.id.my_view)).perform(click()).check(matches(isDisplayed()));
   }
 }
+
+// Notes on instrumentation/UI testing
+/**
+ * 1. ViewMatcher -> locate view matcher in the UI hierarchy. e.g
+ * withId(R.id.my_view) or withText('find some text')
+ */
+
+/**
+ * 2. ViewActions -> perfom specific action or group of actions. e.g
+ * ViewInteraction.perform(click()), doubleClick(), or click(), longClick(),
+ * doubleClick(), swipeDown(), swipeLeft(), swipeRight(), swipeUp(), typeText(),
+ * pressKey(), clearText() etc.
+ */
+
+/**
+ * 3. ViewAssertion -> assert view's state using ViewInteraction.check(assertion
+ * method), where assetion method can be isDisplayed(), isEnabled(), isRoot()
+ */
